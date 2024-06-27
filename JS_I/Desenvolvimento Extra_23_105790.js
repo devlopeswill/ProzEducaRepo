97be83e3bca133433398c0f85a30832c55521d0d
Will Lopes <devlopeswill@mtzol.com.br>
2024-06-26 22:20:56 -0300

    Alterando

--- JS_I/Desenvolvimento Extra_23_105790.js
+++ JS_I/Desenvolvimento Extra_23_105790.js
@@ -7,3 +7,15 @@ seguindo algum dos seguintes três modelos:
 - X é menor que 50
 - X é maior que 50
 numerosDaSorte = [37, 14, 26, 5, 94, 87]*/
+
+const numerosDaSorte = [37, 14, 26, 5, 94, 87];
+
+numerosDaSorte.forEach(numero => {
+  if (numero % 2 === 0 && numero < 50) {
+    console.log(`${numero} é par e menor que 50`);
+  } else if (numero < 50) {
+    console.log(`${numero} é menor que 50`);
+  } else {
+    console.log(`${numero} é maior que 50`);
+  }
+});
